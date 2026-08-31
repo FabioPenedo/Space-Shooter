e7b9a27c473aca33e096f3c6568f0afdcb4b03cd
Fábio Penedo <fabiopenedo21@gmail.com>
2026-08-31 20:01:28 -0300

    Criando tiro do inimigo

--- Assets/Scripts/Inimigo01Controller.cs
+++ Assets/Scripts/Inimigo01Controller.cs
@@ -3,17 +3,47 @@ using UnityEngine;
 public class Inimigo01Controller : MonoBehaviour
 {
     [SerializeField] private float speed = -3f;
+    [SerializeField] private GameObject shot;
+
+    [SerializeField] private float minFireInterval = 0.5f;
+    [SerializeField] private float maxFireInterval = 2f;
 
     private Rigidbody2D rb;
+    private Renderer enemyRenderer;
+    private float fireTimer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        enemyRenderer = GetComponentInChildren<SpriteRenderer>();
+
         rb.linearVelocity = new Vector2(0f, speed);
+
+        fireTimer = GetRandomFireInterval();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Se o inimigo ainda não estiver visível na câmera, não atira
+        if (!enemyRenderer.isVisible)
+            return;
+
+        fireTimer -= Time.deltaTime;
+
+        if (fireTimer <= 0f)
+        {
+            OnFire();
+            fireTimer = GetRandomFireInterval();
+        }
+    }
+
+    private void OnFire()
+    {
+        Instantiate(shot, transform.position, Quaternion.identity);
+    }
+
+    private float GetRandomFireInterval()
+    {
+        return Random.Range(minFireInterval, maxFireInterval);
     }
 }
